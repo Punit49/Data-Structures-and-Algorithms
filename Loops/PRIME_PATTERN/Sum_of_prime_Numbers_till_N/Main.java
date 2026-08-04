d392aa50ef2be33c670ccd77ea74099866138331
Punit49 <therealpunit5@gmail.com>
2026-08-04 17:05:08 +0530

    Sum of prime numbers from 1 to N

---
+++ Loops/PRIME_PATTERN/Sum_of_prime_Numbers_till_N/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean isPrime(int n){
+        if(n == 2) return true;
+        if(n <= 1 || n % 2 == 0) return false;
+
+        for(int i = 3; i * i <= n; i += 2){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int primeSum = 0;
+
+        for(int i = 2; i <= n; i++){
+            if(isPrime(i)) primeSum += i;
+        }
+
+        System.out.println(primeSum);
+    }
+}
